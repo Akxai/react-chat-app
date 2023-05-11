be60c8a0db1e91ad31bea31d7e2415133573f2bf
Akshay Aileni <85357123+Akxai@users.noreply.github.com>
2023-05-11 10:53:29 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Register from "./Register";
 function Home() {
   return (
     <div className="bg-[#a7bcff] h-[100vh] flex items-center justify-center">
-      <div className="border-[1px] border-white border-solid rounded-[10px] w-[65%] h-[80%] flex overflow-hidden">
+      <div className="border-[1px] border-white border-solid rounded-[10px] w-[65%] h-[80%] flex overflow-hidden" id="main">
         <Sidebar />
         <Chat />
       </div>
